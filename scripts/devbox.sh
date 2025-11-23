b84d464cbb628019a902c078a1c73bd0179131ba
AzemaViator <103060968+AzemaViator@users.noreply.github.com>
2025-11-23 07:13:26 +0000

    Fix xgd-open add codex

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -7,6 +7,9 @@ dnf5 copr enable -y atim/starship
 dnf5 -y config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 dnf5 -y config-manager addrepo --from-repofile=https://mise.jdx.dev/rpm/mise.repo
 
+# -.- As this project is trying to be an isolated devbox xdg-utils-distrobox causes major issues
+dnf5 swap xdg-utils-distrobox xdg-utils
+
 # Install updates and packages
 dnf5 update -y
 grep -v '^#' /ctx/packages/devbox.packages | xargs dnf5 install -y
